cc254deff84bf782661352588d0464b5786a4244
TAMIM <saifur.tamim@gmail.com>
2022-02-02 12:06:23 +0600

    fixing PWA issues

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // service worker file
-const staticCacheName = 'calculator-v1.1.1';
+const staticCacheName = 'calculator-v1.1.2';
 const assets = [
     '/scientific-calculator/',
     '/scientific-calculator/index.html',
@@ -8,12 +8,12 @@ const assets = [
     '/scientific-calculator/script/app.js',
     '/scientific-calculator/img/favicon.ico',
     'https://fonts.googleapis.com/css2?family=PT+Sans&family=Charm&family=Tangerine&display=swap',
-'https://fonts.gstatic.com/s/ptsans/v16/jizaRExUiTo99u79D0KExQ.woff2',
-'https://fonts.gstatic.com/s/tangerine/v15/IurY6Y5j_oScZZow4VOxCZZM.woff2',
-'https://fonts.gstatic.com/s/charm/v8/7cHmv4oii5K0MdYoK-4.woff2',
-'https://fonts.gstatic.com/s/ptsans/v16/jizaRExUiTo99u79D0KExQ.woff2',
-'https://fonts.gstatic.com/s/tangerine/v15/IurY6Y5j_oScZZow4VOxCZZM.woff2',
-'https://fonts.gstatic.com/s/charm/v8/7cHmv4oii5K0MdYoK-4.woff2'
+    'https://fonts.gstatic.com/s/ptsans/v16/jizaRExUiTo99u79D0KExQ.woff2',
+    'https://fonts.gstatic.com/s/tangerine/v15/IurY6Y5j_oScZZow4VOxCZZM.woff2',
+    'https://fonts.gstatic.com/s/charm/v8/7cHmv4oii5K0MdYoK-4.woff2',
+    'https://fonts.gstatic.com/s/ptsans/v16/jizaRExUiTo99u79D0KExQ.woff2',
+    'https://fonts.gstatic.com/s/tangerine/v15/IurY6Y5j_oScZZow4VOxCZZM.woff2',
+    'https://fonts.gstatic.com/s/charm/v8/7cHmv4oii5K0MdYoK-4.woff2'
 ];
 
 // install service worker
