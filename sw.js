e9d2ea6ac084f8b21be847d359e6301ccad6d0d2
TAMIM <saifur.tamim@gmail.com>
2022-02-02 19:38:26 +0600

    calculator width height vh to %

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // service worker file
-const staticCacheName = 'calculator-v3.0.0';
+const staticCacheName = 'calculator-v3.1.0';
 const assets = [
     '/scientific-calculator/',
     '/scientific-calculator/index.html',
